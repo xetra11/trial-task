7e45ba8b865a09788ffd061f507f86bf0de461ab
xetra11 <xetra11@outlook.com>
2019-03-09 14:12:39 +0100

    chore(init): init

---
+++ src/main/java/com/xetra11/trial/documentmerger/DocumentmergerApplication.java
@@ -0,0 +1,13 @@
+package com.xetra11.trial.documentmerger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentmergerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DocumentmergerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/xetra11/trial/documentmerger/DocumentmergerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xetra11.trial.documentmerger;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DocumentmergerApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
